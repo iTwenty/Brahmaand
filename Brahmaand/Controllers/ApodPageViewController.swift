6d4b08b8b6be3ad9514fee0366ddf4b45e287bfe
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2021-04-08 16:22:22 +0530

    Remove parchment lib

--- Brahmaand/Controllers/ApodPageViewController.swift
+++ Brahmaand/Controllers/ApodPageViewController.swift
@@ -6,89 +6,49 @@
 //
 
 import UIKit
-import Parchment
 
-struct PagingDateItem: PagingItem, Hashable, Comparable {
-
-    let date: Date
-    let dayText: String?
-    let monthText: String?
-    let yearText: String?
-
-    init(date: Date) {
-        self.date = date
-        self.dayText = Constants.DateFormatters.displayDayFormatter.string(from: date)
-        self.monthText = Constants.DateFormatters.displayMonthFormatter.string(from: date)
-        self.yearText = Constants.DateFormatters.displayYearFormatter.string(from: date)
-    }
-
-    static func < (lhs: PagingDateItem, rhs: PagingDateItem) -> Bool {
-        lhs.date < rhs.date
-    }
-}
-
-class ApodPageViewController: PagingViewController {
-
-    var days: Int = 0
+class ApodPageViewController: UIPageViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        days = Constants.Calendars.apodCalendar.dateComponents([.day], from: Constants.Dates.apodLaunchDate, to: Date()).day!
         self.dataSource = self
-        let nib = UINib(nibName: "ApodDateCell", bundle: nil)
-        self.menuItemSize = .selfSizing(estimatedWidth: 64, height: 64)
-        self.register(nib, for: PagingDateItem.self)
-        self.select(index: days - 1)
+        let apodVc = ApodViewController.fromStoryBoard(date: Date())
+        self.setViewControllers([apodVc], direction: .reverse, animated: false, completion: nil)
     }
 }
 
 
-extension ApodPageViewController: PagingViewControllerDataSource {
+extension ApodPageViewController: UIPageViewControllerDataSource {
 
-    func numberOfViewControllers(in pagingViewController: PagingViewController) -> Int {
-        return days
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let currentDate = (viewController as? ApodViewController)?.date else {
+            return nil
+        }
+        guard let beforeDate = Constants.Calendars.apodCalendar.date(byAdding: .day, value: -1, to: currentDate) else {
+            return nil
+        }
+        let comparison = Constants.Calendars.apodCalendar.compare(Constants.Dates.apodLaunchDate, to: beforeDate, toGranularity: .day)
+        guard comparison == .orderedSame || comparison == .orderedAscending else {
+            return nil
+        }
+        let apodVc = ApodViewController.fromStoryBoard(date: beforeDate)
+        return apodVc
     }
 
-    func pagingViewController(_: PagingViewController, viewControllerAt index: Int) -> UIViewController {
-        let date = Constants.Calendars.apodCalendar.date(byAdding: .day, value: index, to: Constants.Dates.apodLaunchDate)!
-        return ApodViewController.fromStoryBoard(date: date)
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let currentDate = (viewController as? ApodViewController)?.date else {
+            return nil
+        }
+        guard let afterDate = Constants.Calendars.apodCalendar.date(byAdding: .day, value: 1, to: currentDate) else {
+            return nil
+        }
+        let comparison = Constants.Calendars.apodCalendar.compare(afterDate, to: Date(), toGranularity: .day)
+        guard comparison == .orderedSame || comparison == .orderedAscending else {
+            return nil
+        }
+        let apodVc = ApodViewController.fromStoryBoard(date: afterDate)
+        return apodVc
     }
-
-    func pagingViewController(_: PagingViewController, pagingItemAt index: Int) -> PagingItem {
-        let date = Constants.Calendars.apodCalendar.date(byAdding: .day, value: index, to: Constants.Dates.apodLaunchDate)!
-        return PagingDateItem(date: date)
-    }
-
-
-//    func pageViewController(_ pageViewController: UIPageViewController,
-//                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
-//        guard let currentDate = (viewController as? ApodViewController)?.date else {
-//            return nil
-//        }
-//        guard let beforeDate = Constants.Calendars.apodCalendar.date(byAdding: .day, value: -1, to: currentDate) else {
-//            return nil
-//        }
-//        let comparison = Constants.Calendars.apodCalendar.compare(Constants.Dates.apodLaunchDate, to: beforeDate, toGranularity: .day)
-//        guard comparison == .orderedSame || comparison == .orderedAscending else {
-//            return nil
-//        }
-//        let apodVc = ApodViewController.fromStoryBoard(date: beforeDate)
-//        return apodVc
-//    }
-//
-//    func pageViewController(_ pageViewController: UIPageViewController,
-//                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
-//        guard let currentDate = (viewController as? ApodViewController)?.date else {
-//            return nil
-//        }
-//        guard let afterDate = Constants.Calendars.apodCalendar.date(byAdding: .day, value: 1, to: currentDate) else {
-//            return nil
-//        }
-//        let comparison = Constants.Calendars.apodCalendar.compare(afterDate, to: Date(), toGranularity: .day)
-//        guard comparison == .orderedSame || comparison == .orderedAscending else {
-//            return nil
-//        }
-//        let apodVc = ApodViewController.fromStoryBoard(date: afterDate)
-//        return apodVc
-//    }
 }
